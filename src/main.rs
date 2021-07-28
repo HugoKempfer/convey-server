eb82133aee06ca0fea30d02e88bfe9782d0bc54e
Hugo Kempfer <hugkempf@gmail.com>
2021-07-28 10:54:46 +0900

    env_logger: feat -> setup env logger with default values

--- src/main.rs
+++ src/main.rs
@@ -11,13 +11,25 @@ pub mod errors;
 pub mod handlers;
 pub mod utils;
 
+const DEFAULT_PORT: &str = "8080";
+
 async fn try_establish_redis_conn() -> RedisResult<ConnectionManager> {
     let redis_url = std::env::var("REDIS_URL").unwrap_or_else(|_| "redis://127.0.0.1/".to_string());
     redis::aio::ConnectionManager::new(redis::Client::open(redis_url)?).await
 }
 
+fn setup_env_logger() {
+    let env = env_logger::Env::default()
+        .filter_or("CONVEY-LOG-LEVEL", "info")
+        .write_style_or("CONVEY-LOG-LEVEL", "always");
+
+    env_logger::init_from_env(env);
+}
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
+    setup_env_logger();
+    let server_url = std::env::var("CONVEY-URL").unwrap_or_else(|_| "127.0.0.1".to_string());
     info!("Starting server.");
     match try_establish_redis_conn().await {
         Ok(conn) => {
@@ -27,7 +39,7 @@ async fn main() -> std::io::Result<()> {
                     actors::config_actors(cfg, conn.clone());
                 })
             })
-            .bind("127.0.0.1:8080")?
+            .bind(format!("{}:{}", server_url, DEFAULT_PORT))?
             .run()
             .await
         }
